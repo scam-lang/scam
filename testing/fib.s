90e90b997b880cfe820e220d80ac713c776f8ad8
lusth <lusth@38ebec9f-8de8-411d-bf7c-b5c56b76c706>
2011-09-26 04:37:19 +0000

    working
    git-svn-id: svn+ssh://beastie.cs.ua.edu/var/www/svn/scam@125 38ebec9f-8de8-411d-bf7c-b5c56b76c706

--- testing/fib.s
+++ testing/fib.s
@@ -1,7 +1,4 @@
 (println "starting fib.s")
-(println "including fact.s")
-
-(include "fact.s")
 
 (define (fib n)
     (if (< n 2)
@@ -9,11 +6,15 @@
         (+ (fib (- n 1)) (fib (- n 2)))
         )
     )
+
+(println "including fact.s")
+(include "fact.s")
+
 (define x 0)
 (define result nil)
 (define t (time))
 
-(set! x 10)
+(set! x 25)
 (set! result (fib x))
 (println "fib: fib(" x ") is " result)
 (println (- (time) t) " seconds")
