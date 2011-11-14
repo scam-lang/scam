a0a204c97298315e26ab4033dfe7ff6b2c30e58b
lusth <lusth@38ebec9f-8de8-411d-bf7c-b5c56b76c706>
2011-11-14 19:31:12 +0000

    working
    git-svn-id: svn+ssh://beastie.cs.ua.edu/var/www/svn/scam@173 38ebec9f-8de8-411d-bf7c-b5c56b76c706

---
+++ testing/arrays.s
@@ -0,0 +1,17 @@
+(define (allocate* @)
+    (cond
+        ((null? @) nil)
+        (else
+            (define a (allocate (car @)))
+            (for (define i 0) (< i (car @)) (++ i)
+                (setElement a i (apply allocate* (cdr @)))
+                )
+            a
+            )
+        )
+    )
+
+(inspect (allocate 5))
+(inspect (allocate* 5))
+(inspect (allocate* 5 4))
+(inspect (allocate* 5 4 3 2))
