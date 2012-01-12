7a861d5ff2eccd0a7f0d022164679b3e2672543d
lusth <lusth@38ebec9f-8de8-411d-bf7c-b5c56b76c706>
2012-01-12 16:02:11 +0000

    working
    git-svn-id: svn+ssh://beastie.cs.ua.edu/var/www/svn/scam@187 38ebec9f-8de8-411d-bf7c-b5c56b76c706

--- bad-return.s
+++ bad-return.s
@@ -14,5 +14,18 @@
         (println "f was called with " i)
         )
     )
+(define (main2)
+    (println "calling f 16 times")
+    (define i 16)
+    (while (> i 0)
+        (println "calling f with " i)
+        ;(+ (f i) 0)                        ;this works
+        (f i)                               ;this doesn't
+        (println "f was called with " i)
+        (-- i)
+        )
+    )
+
 
 (main)
+(main2)
