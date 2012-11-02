b6422c12174d0ea30847badfa4be14d20bb7f028
kakosta <kakosta@38ebec9f-8de8-411d-bf7c-b5c56b76c706>
2012-11-02 18:00:56 +0000

    changed whitespace
    git-svn-id: svn+ssh://beastie.cs.ua.edu/var/www/svn/scam@211 38ebec9f-8de8-411d-bf7c-b5c56b76c706

--- whitespace.c
+++ whitespace.c
@@ -13,37 +13,55 @@ skipWhiteSpace(PARSER *p)
     int ch;
 
     while ((ch = getNextCharacter(p))
-    && ch != EOF && (isspace(ch) || ch == ';'))
+    && ch != EOF && (isspace(ch) || ch == '/'))
         {
-        if (ch == ';')
-            { 
+        if (ch == '/')
+            {
             ch = getNextCharacter(p);
-            if (ch == '$') /* skip to end of file */
-                {
-                while ((ch = getNextCharacter(p)) && ch != EOF)
-                    continue;
-                }
-            else if (ch == '{') /* skip to close comment */
-                {
-                int prev = ch;
-                int lineNumber = p->line;
-                while ((ch = getNextCharacter(p))
-                && ch != EOF && (prev != ';' || ch != '}'))
-                    {
-                    prev = ch;
-                    }
-                if (ch == EOF)
-                    {
-                    return Fatal("file %s,line %d: unterminated comment\n",
-                            SymbolTable[p->file],lineNumber);
-                    }
-                }
+            if (ch == '/')
+            {
+            ch = getNextCharacter(p);
+            if (ch == '/') /*skip to end of file*/
+              {
+              ch = getNextCharacter(p);
+              while (ch != EOF) //while (!feof(p))
+              {
+              ch = getNextCharacter(p);
+              }
+              }
+            }
             else /* skip to end of line */
                 {
                 while (ch != EOF && ch != '\n')
-                    ch = getNextCharacter(p);
+                ch = getNextCharacter(p);
+                unread(ch);
                 }
             }
+            else if (ch == '*')
+            {
+            int more = 1;
+            while (more)
+                {
+                while ((ch = getNextCharacter(p))
+                && ch != EOF && ch != '*')
+                {
+                    continue;
+                }
+                ch = getNextCharacter(p);
+                if (ch == EOF)
+                    Fatal("SOURCE CODE ERROR: unterminated comment\n");
+                    /* Fatal("SOURCE CODE ERROR\n"
+                        "file %s, line %d: unterminated comment\n",
+                        cellString(0,0,fn), ln); */
+                more = ch != '/';
+                }
+            }
+        else
+        {
+            unread(ch);
+            ch = '/';
+            break;
+        }
         }
 
     return ch;
