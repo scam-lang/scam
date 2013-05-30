94fcc32ec66243bcdd73139982f50e39739b3844
lusth <lusth@38ebec9f-8de8-411d-bf7c-b5c56b76c706>
2013-05-30 23:01:31 +0000

    working
    git-svn-id: svn+ssh://beastie.cs.ua.edu/var/www/svn/scam@240 38ebec9f-8de8-411d-bf7c-b5c56b76c706

--- sway-testing/var.s
+++ sway-testing/var.s
@@ -5,6 +5,27 @@ var y = 2;
 var a,b,c;
 var u = 1,v,w = 3;
 
-function square(x) { x * x; }
+function square(x)
+    {
+    function z(a,b,c) { var x = 3,y = 2; 1; }
+    q = 5;
+    if (m ==== z)
+        {
+        a;
+        }
+    else if (x > y)
+        {
+        var b = 3;
+        }
+    f(q,z,r);
+    x * x;
+    }
 
-ppFunctionDefinition(square,"");
+function bundle(x,y) { this; }
+
+ppClosure(square,"");
+inspect(this);
+inspect(square);
+inspect(+);
+inspect(bundle(1,2));
+inspect(catch(throw('ouch,5)));
